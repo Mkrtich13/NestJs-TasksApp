0f07afd317c456042f2832ab5bda6c132173d169
Mkrtich13 <hovanisianm@gmail.com>
2021-06-02 13:28:11 +0200

    init users repository and entity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository } from './users.repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private readonly usersRepository: UsersRepository,
+  ) {}
+}
